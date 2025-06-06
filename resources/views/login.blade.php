955cd3c9646d5f3b7b14fa7d666959ae36471aea
KUNE23 <nuansajunti8@gmail.com>
2025-06-06 23:02:09 +0700

    fix login mixed content

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,22 +3,22 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="{{ asset('assets/img/logo.png')}}" type="image/x-icon">
+  <link rel="shortcut icon" href="{{ secure_asset('assets/img/logo.png')}}" type="image/x-icon">
   <title>ArsipDokumen | Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/dist/css/alt/adminlte.min.css')}}">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <img src="{{ asset('assets/img/logo.png') }}"></img>
+    <img src="{{ secure_asset('assets/img/logo.png') }}"></img>
     <a href="/"><b>Arsip</b>Dokumen</a>
   </div>
   <!-- /.login-logo -->
@@ -87,10 +87,10 @@
   </script>
   
 <!-- jQuery -->
-<script src="{{ asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('assets/dist/js/adminlte.min.js')}}"></script>
+<script src="{{ secure_asset('assets/dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>

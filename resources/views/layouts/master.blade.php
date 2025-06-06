439085ae4997265153c88c1348ddda0931331021
KUNE23 <nuansajunti8@gmail.com>
2025-06-06 22:50:06 +0700

    fix css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,30 +3,30 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="{{ asset('assets/img/logo.png')}}" type="image/x-icon">
+  <link rel="shortcut icon" href="{{ secure_asset('assets/img/logo.png')}}" type="image/x-icon">
   <title>ArsipDokumen @yield('tittle')</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
   <!-- iCheck -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/jqvmap/jqvmap.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/jqvmap/jqvmap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('assets/css/alt/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/css/alt/adminlte.min.css')}}">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/daterangepicker/daterangepicker.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/daterangepicker/daterangepicker.css')}}">
   <!-- summernote -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/summernote/summernote-bs4.min.css')}}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/summernote/summernote-bs4.min.css')}}">
   <!-- datatable -->
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 </head>
@@ -35,7 +35,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('assets/img/logo.png')}}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ secure_asset('assets/img/logo.png')}}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -57,7 +57,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
-      <img src="{{ asset('assets/img/logo-kreditplus.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ secure_asset('assets/img/logo-kreditplus.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Arsip Dokumen</span>
     </a>
 
@@ -66,7 +66,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('assets/img/avatar2.png')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ secure_asset('assets/img/avatar2.png')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="/" class="d-block">
@@ -103,9 +103,9 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{ asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/jquery/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{ asset('assets/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
@@ -137,24 +137,24 @@
   @endif
   </script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- ChartJS -->
-<script src="{{ asset('assets/plugins/chart.js/Chart.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/chart.js/Chart.min.js')}}"></script>
 <!-- Sparkline -->
-<script src="{{ asset('assets/plugins/sparklines/sparkline.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/sparklines/sparkline.js')}}"></script>
 <!-- jQuery Knob Chart -->
-<script src="{{ asset('assets/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
 <!-- daterangepicker -->
-<script src="{{ asset('assets/plugins/moment/moment.min.js')}}"></script>
-<script src="{{ asset('assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/moment/moment.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
 <!-- Summernote -->
-<script src="{{ asset('assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
 <!-- overlayScrollbars -->
-<script src="{{ asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="{{ secure_asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('assets/js/adminlte.js')}}"></script>
+<script src="{{ secure_asset('assets/js/adminlte.js')}}"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
      <script>
     $(document).ready(function(){
@@ -163,8 +163,8 @@
     </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<!-- <script src="{{ asset('assets/js/demo.js')}}"></script> -->
+<!-- <script src="{{ secure_asset('assets/js/demo.js')}}"></script> -->
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ asset('assets/js/pages/dashboard.js')}}"></script>
+<script src="{{ secure_asset('assets/js/pages/dashboard.js')}}"></script>
 </body>
 </html>

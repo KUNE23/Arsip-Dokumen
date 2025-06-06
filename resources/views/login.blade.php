a0c7e3ee9ae1f385190790a770f82e93a0f51b3f
KUNE23 <nuansajunti8@gmail.com>
2025-06-07 06:13:25 +0700

    fix again

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,13 +7,13 @@
   <title>ArsipDokumen | Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" media="all" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/dist/css/adminlte.min.css')}}">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">

6f7ce5acc9c2ca14cec6b2a8f5c729b525107703
KUNE23 <nuansajunti8@gmail.com>
2025-06-06 22:53:17 +0700

    fix css2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,24 +9,24 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
   <!-- iCheck -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/jqvmap/jqvmap.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/jqvmap/jqvmap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/css/alt/adminlte.min.css')}}">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/css/alt/adminlte.min.css')}}">
+  <link rel="stylesheet" media="all" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/daterangepicker/daterangepicker.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/daterangepicker/daterangepicker.css')}}">
   <!-- summernote -->
-  <link rel="stylesheet" href="{{ secure_asset('assets/plugins/summernote/summernote-bs4.min.css')}}">
+  <link rel="stylesheet" media="all" href="{{ secure_asset('assets/plugins/summernote/summernote-bs4.min.css')}}">
   <!-- datatable -->
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 </head>
